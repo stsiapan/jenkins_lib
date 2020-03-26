efb2cb4fbc5fa63a553011bafd4c5b8eac237a69
mer <123@123.com>
2020-03-26 14:27:41 +0300

    0.2

---
+++ vars/allPipeline.groovy
@@ -0,0 +1,43 @@
+def call(body) {
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+pipeline {
+  agent any
+
+  stages {
+    stage('Checkout') {
+      steps {
+        checkout scm
+      }
+    }
+    
+    stage('get dockerfile') {
+      steps {
+        createDockerfile()
+      }
+    }
+    
+    stage('Log error') {
+      steps {
+        logError 'panic mode!'
+      }
+    }
+    
+    stage('ls') {
+      steps {
+        sh 'ls -la; pwd'
+      }
+    }
+    
+    stage('Build') {
+      steps {
+        dockerCmd 'version'
+        dockerCmd 'build -t ealebed/hellonode:latest .'
+        dockerCmd 'image ls'
+      }
+    }
+  }
+}
